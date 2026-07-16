b7377d30fe9d1184102eed36444655abea5f975f
Rostyslav Bodnariuk <rostislav.bodnaryuk@gmail.com>
2026-07-16 07:53:11 +0300

    connect frontend to backend + CORS

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,6 +6,14 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("frontend", policy =>
+        policy.WithOrigins("http://localhost:5173")  // кому разрешаем
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,8 +24,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("frontend");
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
